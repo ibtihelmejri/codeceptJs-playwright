ef2126a59a3df94225eff43828e89baf5634dadd
ibtihel <mejriibtihel90@gmail.com>
2022-11-27 13:03:34 +0100

    create test for register account

--- login_test.js
+++ login_test.js
@@ -1,10 +1,10 @@
 Feature('login');
 
-Scenario('test something', ({ I }) => {
+Scenario('authentifacation user', ({ I }) => {
 I.amOnPage('/signin');
 I.see('Se connecter');
-I.fillField("//input[@id='email']", "mejrijihen@gmail.com");
-I.fillField("#password","Jihen123*");
+I.fillField("//input[@id='email']", "jean.dujardin@gmail.com");
+I.fillField("#password","Jean123*");
 I.click("//div[@class='mt-3']");
 I.see("Gestion des utilisateurs");
 I.dontSee("Se connecter")
